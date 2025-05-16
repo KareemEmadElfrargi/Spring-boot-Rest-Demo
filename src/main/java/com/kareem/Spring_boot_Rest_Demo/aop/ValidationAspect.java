150e24ad167379b59b39340eddec126dfc483f35
Kareem Emad <@gmail.com>
2025-05-16 16:13:21 +0300

    Validation Aspect

---
+++ src/main/java/com/kareem/Spring_boot_Rest_Demo/aop/ValidationAspect.java
@@ -0,0 +1,33 @@
+package com.kareem.Spring_boot_Rest_Demo.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+
+public class ValidationAspect {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ValidationAspect.class);
+
+    @Around("execution(* com.kareem.Spring_boot_Rest_Demo.service.JobService.*(..)) && args(postId)")
+    public Object validateAndUpdate(ProceedingJoinPoint proceedingJoinPoint, int postId) throws Throwable{
+
+        if (postId<0){
+            LOGGER.info("Post id is negative , updating it");
+            postId = -postId;
+            LOGGER.info("new value : "+postId);
+
+        }
+        Object object = proceedingJoinPoint.proceed(new Object[]{postId});
+
+
+
+        return object;
+    }
+
+
+}
